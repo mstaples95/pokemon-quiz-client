ceb3b0e4920646a945f86a55db8f7d7a5dd2526b
Eric Lo <eric.lo.ca@outlook.com>
2023-09-21 09:50:25 -0700

    merge fixes

--- Client/pokemon-quiz/src/pages/Home/Home.js
+++ Client/pokemon-quiz/src/pages/Home/Home.js
@@ -43,6 +43,7 @@ const Home = () => {
     <>
       <main className="main">
         <PokeDisplay />
+        {localPokemonData && <PokeInfo localdata={localPokemonData} />}
         <PokeInfo />
       </main>
       {pokemonData && <PokeCard data={pokemonData} />}
