83807bfebd98e4472affed46288e0b0198e2e8c0
ISENDIL-SUYUN\isend <isendil_1@yahoo.com>
2023-09-21 15:32:42 -0400

    passing Eric's function

--- Client/pokemon-quiz/src/components/PokeInfo/Poke-Input-Info.js
+++ Client/pokemon-quiz/src/components/PokeInfo/Poke-Input-Info.js
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 import "./poke-info.scss";
 
-const PokeInfo = ({ localdata }) => {
+const PokeInfo = ({ localdata, validateAnswers }) => {
   // const [refresh, setRefresh] = useState(false);
   // console.log(localdata);
   // const { id, answers, question, typeAnswer } = localdata;
@@ -11,69 +11,6 @@ const PokeInfo = ({ localdata }) => {
   // const toggleRefresh = () => {
   //   setRefresh(!refresh);
   // };
-
-  const validateAnswers = () => {
-    let UserAnswers = [];
-    let ElementList = [
-      "Normal",
-      "Fire",
-      "Water",
-      "Grass",
-      "Electric",
-      "Ice",
-      "Fighting",
-      "Poison",
-      "Ground",
-      "Flying",
-      "Psychic",
-      "Bug",
-      "Rock",
-      "Ghost",
-      "Dragon",
-      "Dark",
-      "Steel",
-      "Fairy",
-    ];
-    let TypeCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let selected = document.getElementsByTagName("input");
-    // Collects all user answers into UserAnswers
-    for (let i = 0; i < selected.length; i++) {
-      if ((selected[i].type = "radio")) {
-        if (selected[i].checked) UserAnswers.push(selected[i].value);
-      }
-    }
-    // Counts results of element count
-    for (let i = 0; i < UserAnswers.length; i++) {
-      if (UserAnswers[i] === "Normal") TypeCount[0]++;
-      if (UserAnswers[i] === "Fire") TypeCount[1]++;
-      if (UserAnswers[i] === "Water") TypeCount[2]++;
-      if (UserAnswers[i] === "Grass") TypeCount[3]++;
-      if (UserAnswers[i] === "Electric") TypeCount[4]++;
-      if (UserAnswers[i] === "Ice") TypeCount[5]++;
-      if (UserAnswers[i] === "Fighting") TypeCount[6]++;
-      if (UserAnswers[i] === "Poison") TypeCount[7]++;
-      if (UserAnswers[i] === "Ground") TypeCount[8]++;
-      if (UserAnswers[i] === "Flying") TypeCount[9]++;
-      if (UserAnswers[i] === "Psychic") TypeCount[10]++;
-      if (UserAnswers[i] === "Bug") TypeCount[11]++;
-      if (UserAnswers[i] === "Rock") TypeCount[12]++;
-      if (UserAnswers[i] === "Ghost") TypeCount[13]++;
-      if (UserAnswers[i] === "Dragon") TypeCount[14]++;
-      if (UserAnswers[i] === "Dark") TypeCount[15]++;
-      if (UserAnswers[i] === "Steel") TypeCount[16]++;
-      if (UserAnswers[i] === "Fairy") TypeCount[17]++;
-    }
-    console.log(UserAnswers);
-    console.log(TypeCount);
-    let pos = 0;
-    //Find largest count of element
-    for (let i = 0; i < TypeCount.length; i++) {
-      if (TypeCount[pos] < TypeCount[i]) pos = i;
-    }
-    alert(`You are a ${ElementList[pos]} Pokemon!`);
-    const result = ElementList[pos];
-    // toggleRefresh();
-  };
 
   for (let i = 0; i < NumberOfQuestions; i++) {
     filterData.push(localdata[Math.floor(Math.random() * localdata.length)]);
--- Client/pokemon-quiz/src/pages/Home/Home.js
+++ Client/pokemon-quiz/src/pages/Home/Home.js
@@ -12,6 +12,72 @@ import Jukebox from "../../components/Jukebox/Jukebox";
 const Home = () => {
   const [pokemonData, setPokemonData] = useState(null);
   const [localPokemonData, setLocalPokemonData] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const validateAnswers = () => {
+    let UserAnswers = [];
+    let ElementList = [
+      "Normal",
+      "Fire",
+      "Water",
+      "Grass",
+      "Electric",
+      "Ice",
+      "Fighting",
+      "Poison",
+      "Ground",
+      "Flying",
+      "Psychic",
+      "Bug",
+      "Rock",
+      "Ghost",
+      "Dragon",
+      "Dark",
+      "Steel",
+      "Fairy",
+    ];
+    let TypeCount = Array(18).fill(0);
+    let selected = document.getElementsByTagName("input");
+    // Collects all user answers into UserAnswers
+    for (let i = 0; i < selected.length; i++) {
+      if (selected[i].type === "radio") {
+        if (selected[i].checked) UserAnswers.push(selected[i].value);
+      }
+    }
+    // Counts results of element count
+    for (let i = 0; i < UserAnswers.length; i++) {
+      if (UserAnswers[i] === "Normal") TypeCount[0]++;
+      if (UserAnswers[i] === "Fire") TypeCount[1]++;
+      if (UserAnswers[i] === "Water") TypeCount[2]++;
+      if (UserAnswers[i] === "Grass") TypeCount[3]++;
+      if (UserAnswers[i] === "Electric") TypeCount[4]++;
+      if (UserAnswers[i] === "Ice") TypeCount[5]++;
+      if (UserAnswers[i] === "Fighting") TypeCount[6]++;
+      if (UserAnswers[i] === "Poison") TypeCount[7]++;
+      if (UserAnswers[i] === "Ground") TypeCount[8]++;
+      if (UserAnswers[i] === "Flying") TypeCount[9]++;
+      if (UserAnswers[i] === "Psychic") TypeCount[10]++;
+      if (UserAnswers[i] === "Bug") TypeCount[11]++;
+      if (UserAnswers[i] === "Rock") TypeCount[12]++;
+      if (UserAnswers[i] === "Ghost") TypeCount[13]++;
+      if (UserAnswers[i] === "Dragon") TypeCount[14]++;
+      if (UserAnswers[i] === "Dark") TypeCount[15]++;
+      if (UserAnswers[i] === "Steel") TypeCount[16]++;
+      if (UserAnswers[i] === "Fairy") TypeCount[17]++;
+    }
+    console.log(UserAnswers);
+    console.log(TypeCount);
+    let pos = 0;
+    //Find largest count of element
+    for (let i = 0; i < TypeCount.length; i++) {
+      if (TypeCount[pos] < TypeCount[i]) pos = i;
+    }
+    alert(`You are a ${ElementList[pos]} Pokemon!`);
+    setResult(ElementList[pos]);
+    // toggleRefresh();
+  };
+
+  validateAnswers.results = result;
 
   useEffect(() => {
     const getAllPokemon = async () => {
@@ -42,8 +108,13 @@ const Home = () => {
   return (
     <>
       <main className="main">
-        <PokeDisplay />
-        {localPokemonData && <PokeInfo localdata={localPokemonData} />}
+        <PokeDisplay results={result} />
+        {localPokemonData && (
+          <PokeInfo
+            localdata={localPokemonData}
+            validateAnswers={validateAnswers}
+          />
+        )}
       </main>
       {pokemonData && <PokeCard data={pokemonData} />}
       <Jukebox />
