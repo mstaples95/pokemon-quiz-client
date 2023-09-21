ed8b7ab60a4ed54214929d26f987407f89946269
ISENDIL-SUYUN\isend <isendil_1@yahoo.com>
2023-09-21 15:53:15 -0400

    fixed minor codes

--- Client/pokemon-quiz/src/components/Header/Header.js
+++ Client/pokemon-quiz/src/components/Header/Header.js
@@ -20,6 +20,15 @@ const Header = () => {
           <li className="header__menu-item">About</li>
         </ul>
       </div>
+      <div className="music-player">
+        <iframe
+          className="jukebox"
+          title="jukebox"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1692262794&color=%23ff0019&auto_play=false&hide_related=false&show_comments=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
     </header>
   );
 };
